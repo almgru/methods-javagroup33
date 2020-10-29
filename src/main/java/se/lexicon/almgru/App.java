d2460a5e71db586e3269e3d9aa4da2254701030e
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2020-10-29 10:37:10 +0100

    Renames printOut to welcomeMessage

--- src/main/java/se/lexicon/almgru/App.java
+++ src/main/java/se/lexicon/almgru/App.java
@@ -17,12 +17,12 @@ public class App
     // Needs to be inside class or interface.
     public static void name() { }
 
-    public static void printOut() {
+    public static void welcomeMessage() {
         System.out.println("Hello World - From Method");
     }
 
     public static void printOutName(String name) {
-        System.out.print("Hi, the name is: ");
+        System.out.print("The name is: ");
         System.out.println(name);
     }
 }
